0f517c2a35d38331c3e82770102ebb8041b36b59
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-07 20:50:35 -0300

    Add project files.

---
+++ AreBothStringsEquals/Program.cs
@@ -0,0 +1,24 @@
+﻿string firstString, secondString;
+GetData();
+UserAnswer();
+
+
+bool CompareStrings(string x, string y)
+{
+    if (x.Equals(y)) return true;
+    else return false;      
+}
+void GetData()
+{
+    Console.WriteLine("Put 1st string to compare:");
+    firstString = Console.ReadLine();
+    Console.WriteLine("Put 2nd string to compare");
+    secondString = Console.ReadLine();
+}
+void UserAnswer()
+{
+    if (CompareStrings(firstString, secondString)) Console.WriteLine("The both" +
+        " strings are equals\n({0} == {1})", firstString, secondString);
+    else Console.WriteLine("The both strings are not equals\n" +
+            "({0} != {1})", firstString, secondString);
+}
